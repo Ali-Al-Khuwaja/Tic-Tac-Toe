fe27841da7e45f4bc1746867ab513c363082e3bd
Ali-Al-Khuwaja <ali.al.khuwaja@gmail.com>
2024-04-03 00:46:01 +0300

    initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+// first i need to render a 3 x 3 array and i want them to be be able to display all of it's values 
+// keep in mind to use these 1 . closure  2. factory functions 3. module pattern
+
+function displayInConsole(){
+    // place holder
+    const game = ['x','x','x','o','o','o','x','x','x'];
+    console.log(
+        game[0],'|',game[1],'|',game[2],'\n',game[3],'|',game[4],'|',game[5],'\n',game[6],'|',game[7],'|',game[8]
+    );
+};
+displayInConsole();
+
+
+
+
