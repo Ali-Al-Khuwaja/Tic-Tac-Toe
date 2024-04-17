8d13f15c0423309f87d1b3df8f727339da6133c8
Ali-Al-Khuwaja <ali.al.khuwaja@gmail.com>
2024-04-17 22:34:53 +0300

    i got something working

--- main.js
+++ main.js
@@ -1,118 +1,111 @@
-// // first i need to render a 3 x 3 array and i want them to be be able to display all of it's values 
-// // keep in mind to use these 1 . closure  2. factory functions 3. module pattern
+//  keep in mind to use these 1 . closure  2. factory functions 3. module pattern
 
-// const gameModule = (()=>{
-//     // IIFE module pattern , this one is like a closure but it does remember values 
-//     const game = ['/','/','/','/','/','/','/','/','/']; //this has a fixed size.
-//     let counter = 0 ;
-//     function checkIfWon(){
-//         // here is every combo that can be used to win in this game , hey , as long as it works why should we care and over engineer it ? 
-//         if(game[0]==='x' && game[1]==='x' && game[2]==='x'){
-//             console.log('x has won');
-//         }
-//         else if (game[3]==='x' && game[4]==='x' && game[5]==='x'){
-//             console.log('x has won');
-//         }
-//         else if (game[6]==='x' && game[7]==='x' && game[8]==='x'){
-//             console.log('x has won');
-//         }
-//         else if (game[0]==='x' && game[4]==='x' && game[8]==='x'){
-//             console.log('x has won');
-//         }
-//         else if (game[2]==='x' && game[4]==='x' && game[6]==='x'){
-//             console.log('x has won');
-//         }
-//         else if(game[0]==='o' && game[1]==='o' && game[2]==='o'){
-//             console.log('o has won');
-//         }
-//         else if (game[3]==='o' && game[4]==='o' && game[5]==='o'){
-//             console.log('o has won');
-//         }
-//         else if (game[6]==='o' && game[7]==='o' && game[8]==='o'){
-//             console.log('o has won');
-//         }
-//         else if (game[0]==='o' && game[4]==='o' && game[8]==='o'){
-//             console.log('o has won');
-//         }
-//         else if (game[2]==='o' && game[4]==='o' && game[6]==='o'){
-//             console.log('o has won');
-//         }
-    
-//     }
-//     const displayInConsole = () => {
-//         const playerChoice = prompt('what is your choice ?');
-//         console.log(playerChoice);
-//         let marker = 'x';
-//         if(counter%2!==0){
-//             marker = 'o';
-//         }
-//         if(playerChoice==='1'){
-//             console.clear();
-//             game[0] = marker
-//             console.log(game[0],'|',game[1],'|',game[2],'\n',game[3],'|',game[4],'|',game[5],'\n',game[6],'|',game[7],'|',game[8]);
-//             counter++
-//             checkIfWon()
-//         }else if (playerChoice==='2'){
-//             console.clear();
-//             game[1] = marker
-//             console.log(game[0],'|',game[1],'|',game[2],'\n',game[3],'|',game[4],'|',game[5],'\n',game[6],'|',game[7],'|',game[8]);
-//             counter++
-//             checkIfWon()
-//         }else if (playerChoice==='3'){
-//             console.clear();
-//             game[2] = marker
-//             console.log(game[0],'|',game[1],'|',game[2],'\n',game[3],'|',game[4],'|',game[5],'\n',game[6],'|',game[7],'|',game[8]);
-//             counter++
-//             checkIfWon()
-//         }else if (playerChoice=='4'){
-//             console.clear();
-//             game[3] = marker
-//             console.log(game[0],'|',game[1],'|',game[2],'\n',game[3],'|',game[4],'|',game[5],'\n',game[6],'|',game[7],'|',game[8]);
-//             counter++
-//             checkIfWon()
-//         }else if (playerChoice==='5'){
-//             console.clear();
-//             game[4] = marker
-//             console.log(game[0],'|',game[1],'|',game[2],'\n',game[3],'|',game[4],'|',game[5],'\n',game[6],'|',game[7],'|',game[8]);
-//             counter++
-//             checkIfWon()
-//         }else if (playerChoice==='6'){
-//             console.clear();
-//             game[5] = marker
-//             console.log(game[0],'|',game[1],'|',game[2],'\n',game[3],'|',game[4],'|',game[5],'\n',game[6],'|',game[7],'|',game[8]);
-//             counter++
-//             checkIfWon()
-//         }else if (playerChoice==='7'){
-//             console.clear();
-//             game[6] = marker
-//             console.log(game[0],'|',game[1],'|',game[2],'\n',game[3],'|',game[4],'|',game[5],'\n',game[6],'|',game[7],'|',game[8]);
-//             counter++
-//             checkIfWon()
-//         }else if (playerChoice==='8'){
-//             console.clear();
-//             game[7] = marker
-//             console.log(game[0],'|',game[1],'|',game[2],'\n',game[3],'|',game[4],'|',game[5],'\n',game[6],'|',game[7],'|',game[8]);
-//             counter++
-//             checkIfWon()
-//         }else if (playerChoice==='9'){
-//             console.clear();
-//             game[8] = marker
-//             console.log(game[0],'|',game[1],'|',game[2],'\n',game[3],'|',game[4],'|',game[5],'\n',game[6],'|',game[7],'|',game[8]);
-//             counter++
-//             checkIfWon()
-//         }else{
-//             console.log('pick from 1 to 9 please');
-//         };
-//     }
-//     return displayInConsole;
+//Add event listeners to each node and a unique ID that can be used to know on what cell to draw using display()
+// each cell has it's own eventListener function that contains a unique ID
+const game = ['/','/','/','/','/','/','/','/','/']; //this has a fixed size.
+const cell = document.querySelectorAll('.cell');
+let cellID = 0 ;
+cell.forEach((element)=>{
+    element.addEventListener('click' , ()=>{
+        cellID = cellID + 1 ; // 1 then 2 then 3  ...
+        element.id = cellID;
+        display(cellID);
+    });
+});
+// use the ID to draw an X on that cell that share the same ID in the array
 
-// })();
-
-function start(){
-    for(let i=0;8>i;i++){
-        alert('YOU ARE X');
-        const play = gameModule;
-        play();
-    };
+let counter = 0 ;
+function display(ID){
+    let cell = document.getElementById(ID);
+    let marker = 'x';
+    let playerChoice = ID ;
+    if(counter%2!==0){
+        marker = 'o';
+    }
+    if(playerChoice===1){
+        game[0] = marker
+        cell.textContent = `${marker}`;
+        counter++
+        checkIfWon()
+    }else if (playerChoice===2){
+        game[1] = marker
+        cell.innerHTML = `${marker}`;
+        counter++
+        checkIfWon()
+    }else if (playerChoice===3){
+        game[2] = marker
+        cell.innerHTML = `${marker}`;
+        counter++
+        checkIfWon()
+    }else if (playerChoice==4){
+        game[3] = marker
+        cell.innerHTML = `${marker}`;
+        counter++
+        checkIfWon()
+    }else if (playerChoice===5){
+        game[4] = marker
+        cell.innerHTML = `${marker}`;
+        counter++
+        checkIfWon()
+    }else if (playerChoice===6){
+        game[5] = marker
+        cell.innerHTML = `${marker}`;
+        counter++
+        checkIfWon()
+    }else if (playerChoice===7){
+        game[6] = marker
+        cell.innerHTML = `${marker}`;
+        counter++
+        checkIfWon()
+    }else if (playerChoice===8){
+        game[7] = marker
+        cell.innerHTML = `${marker}`;
+        counter++
+        checkIfWon()
+    }else if (playerChoice===9){
+        game[8] = marker
+        cell.innerHTML = `${marker}`;
+        counter++
+        checkIfWon()
+    }
 };
 
+// function gameModule(){
+//     // IIFE module pattern , this one is like a closure but it does remember values 
+//     return displayInConsole;
+// };
+
+function checkIfWon(){
+    // here is every combo that can be used to win in this game , hey , as long as it works why should we care and over engineer it ? 
+    if(game[0]==='x' && game[1]==='x' && game[2]==='x'){
+        console.log('x has won');
+    }
+    else if (game[3]==='x' && game[4]==='x' && game[5]==='x'){
+        console.log('x has won');
+    }
+    else if (game[6]==='x' && game[7]==='x' && game[8]==='x'){
+        console.log('x has won');
+    }
+    else if (game[0]==='x' && game[4]==='x' && game[8]==='x'){
+        console.log('x has won');
+    }
+    else if (game[2]==='x' && game[4]==='x' && game[6]==='x'){
+        console.log('x has won');
+    }
+    else if(game[0]==='o' && game[1]==='o' && game[2]==='o'){
+        console.log('o has won');
+    }
+    else if (game[3]==='o' && game[4]==='o' && game[5]==='o'){
+        console.log('o has won');
+    }
+    else if (game[6]==='o' && game[7]==='o' && game[8]==='o'){
+        console.log('o has won');
+    }
+    else if (game[0]==='o' && game[4]==='o' && game[8]==='o'){
+        console.log('o has won');
+    }
+    else if (game[2]==='o' && game[4]==='o' && game[6]==='o'){
+        console.log('o has won');
+    }
+
+}
